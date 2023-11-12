8b2d8bd61808256ffec251303a6745cd8a67975c
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-11-12 13:27:56 -0800

    I have made updates to the functions code by adding comments

--- function_code.c
+++ function_code.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * char_func - This maintains the structure and execution of char
+ * @list_words: This holds the variable args
+ * Return: Returns number of char
+ */
 int char_func(va_list list_words)
 {
 	int tally_character = 0;
@@ -11,7 +15,11 @@ int char_func(va_list list_words)
 
 	return (1);
 }
-
+/**
+ * string_func - This maintains structure and execution of string
+ * @list_words: This holds the variable args
+ * Return: Returns number of char
+ */
 int string_func(va_list list_words)
 {
 	int tally_character = 0;
@@ -29,12 +37,17 @@ int string_func(va_list list_words)
 
 	return (tally_character);
 }
-
+/**
+ * integer_func - This maintains structure and execution of integer
+ * @list_words: This holds the variable args
+ * Return: Returns number of char
+ */
 int integer_func(va_list list_words)
 {
 	int tally_character = 0, length_Of_Str = 0;
 	int d_int = va_arg(list_words, int);
 	char id_buff[12];
+
 	intConversion(d_int, id_buff);
 
 	while (id_buff[length_Of_Str] != '\0')
@@ -42,6 +55,8 @@ int integer_func(va_list list_words)
 		length_Of_Str++;
 	}
 	write(1, id_buff, length_Of_Str);
+
 	tally_character += length_Of_Str;
-        return (length_Of_Str);
+
+	return (length_Of_Str);
 }
